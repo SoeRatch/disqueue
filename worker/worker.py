b5a86f95e55a36bf17d199f1b0af3f8688f0b09c
SoeRatch <surajkoho@gmail.com>
2025-06-11 12:16:44 +0530

    fixing retry logic

--- worker/worker.py
+++ worker/worker.py
@@ -3,7 +3,7 @@
 import json
 import time
 from task_queues.redis_queue import (
-    r, JOB_STREAM, mark_job_status,
+    r, JOB_STREAM, get_job_status, mark_job_status,
     increment_retry_count, clear_retry_count,
     MAX_RETRIES
 )
@@ -16,7 +16,7 @@ STATUS_FAILED = "failed"
 
 def process(payload: dict):
     print("Processing:", payload)
-    time.sleep(2)
+    time.sleep(20)
     if payload.get("fail"):
         raise Exception("Simulated failure")
 
@@ -42,20 +42,26 @@ def handle_failure(job_id: str, payload: dict, error: Exception):
 
 def start_worker():
     print("Worker started...")
+    last_id = "$" # start with new entries
     while True:
         try:
-            # Continuously listens to Redis Stream (XREAD) for new jobs.
-            res = r.xread({JOB_STREAM: "$"}, block=5000, count=1)
+            # Continuously listens to Redis Stream (XREAD) .
+            res = r.xread({JOB_STREAM: last_id}, block=5000, count=1)
             if not res:
                 continue
 
             _, messages = res[0]
+            last_id = messages[-1][0] # latest message ID
             for msg_id, msg_data in messages:
                 job_id = msg_data.get("job_id")
                 payload = json.loads(msg_data.get("payload", "{}"))
 
                 print(f"\nReceived job {job_id}")
-                mark_job_status(job_id, "in_progress")
+
+                current_status = get_job_status(job_id)
+                if current_status != STATUS_RETRYING:
+                    mark_job_status(job_id, STATUS_IN_PROGRESS)
+
                 try:
                     process(payload)
                     handle_success(job_id)
