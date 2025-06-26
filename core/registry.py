1bcabd33d416f7bfb0c6777a2f1f7570e09cb9dc
SoeRatch <surajkoho@gmail.com>
2025-06-27 00:42:48 +0530

    Added comments

--- core/registry.py
+++ core/registry.py
@@ -3,4 +3,8 @@ from core.queue import DisqueueQueue
 from infrastructure.redis_conn import redis_client
 
 def get_registered_queues():
+    """
+    Loads statically registered queues from config.queue_registry
+    and returns a list of initialized DisqueueQueue instances.
+    """
     return [DisqueueQueue(config, redis_client) for config in REGISTERED_QUEUES]
