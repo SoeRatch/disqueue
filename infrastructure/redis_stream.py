564b5ee3ecf677bfb5b59b5083773c01c070062d
SoeRatch <surajkoho@gmail.com>
2025-06-27 14:08:19 +0530

    Job redis stream refactored to handle job store

--- infrastructure/redis_stream.py
+++ infrastructure/redis_stream.py
@@ -1,33 +1,34 @@
 # infrastructure/redis_stream.py
 
 import logging
-from infrastructure.redis_queue import get_last_id, set_last_id
+from infrastructure.redis_job_store import RedisJobStore
 
 class QueueStreamManager:
-    def __init__(self, queue):
+    def __init__(self, queue, job_store: RedisJobStore):
         self.queue = queue
-        self.client = queue.client
+        self.job_store = job_store
         self.streams = queue.streams
-        self.last_ids = {stream: get_last_id(stream) for stream in self.streams} # Get last_ids of all priority streams
+        # Get last_ids of all priority streams
+        self.last_ids = {stream: self.job_store.get_last_id(stream) for stream in self.streams} 
 
     def get_next_job(self):
         """
-        Reads one job from the highest priority stream with available jobs.
+        Fetch next job from any available priority stream and tries streams in order of priority.
         Returns: (stream, msg_id, msg_data) or None
         """
         for stream in self.streams:
             try:
                 # Read from the current priority stream using XREAD.
-                # # r.xread({stream: ID}) returns messages with IDs strictly greater than the given ID.
+                # # xread({stream: ID}) returns messages with IDs strictly greater than the given ID.
                 # # This ensures the same message is not processed twice.
-                res = self.client.xread({stream: self.last_ids[stream]}, block=1000, count=1)
+                res = self.job_store.client.xread({stream: self.last_ids[stream]}, block=1000, count=1)
                 if not res:
                     continue
                 _, messages = res[0]
                 msg_id, msg_data = messages[0]
                 return stream, msg_id, msg_data
             except Exception as e:
-                logging.error(f"Stream read error on {stream}: {e}")
+                logging.error(f"[stream] Error reading from stream {stream}: {e}")
                 continue
         return None
 
@@ -36,4 +37,4 @@ class QueueStreamManager:
         After successful or skipped processing, update the last_id.
         """
         self.last_ids[stream] = msg_id
-        set_last_id(stream, msg_id)
+        self.job_store.set_last_id(stream, msg_id)
