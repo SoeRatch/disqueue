c7089b2df306b93f1e9b13ee8e8a78e58292ede0
SoeRatch <surajkoho@gmail.com>
2025-06-28 01:58:56 +0530

    Deleted older files after refactoring to new folder structure

--- config/status_codes.py
+++
@@ -1,8 +0,0 @@
-# config/status_codes.py
-
-STATUS_QUEUED = "queued"
-STATUS_IN_PROGRESS = "in_progress"
-STATUS_RETRYING = "retrying"
-STATUS_COMPLETED = "completed"
-STATUS_FAILED = "failed"
-STATUS_CANCELLED = "cancelled"
